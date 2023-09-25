2fac7b35651c8f5527bc6a1312dc7dc480540208
VISHALUNNI <vishalunni87@gmail.com>
2023-09-25 21:07:18 +0530

    file added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './assets/login.jsx'; 
+import './app.css'
+
+const App=()=>{
+	return(
+		<div>
+		<Login/>
+		</div>
+	)
+}
+
+export default App 
---
+++ src/assets/login.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import user_icon from './person.png'
+import email_icon from './email.png'
+import password_icon from './password.png'
+import './login.css'
+
+const login = () => {
+
+  const [action,setAction] = useState("Sign Up");
+
+  return (
+	<div className='container'>
+    <div className="header">
+      <div className="text">{action}</div>
+        <div className="underline"></div>
+    </div>
+    <div className="inputs">
+      <div className="input">
+        <img src={user_icon} alt='' />
+        <input type="text" placeholder='Name'/>
+      </div>
+      <div className="input">
+        <img src={email_icon} alt='' />
+        <input type="email" placeholder='Email-Id'/>
+      </div>
+      <div className="input">
+        <img src={password_icon} alt='' />
+        <input type="password" placeholder='Password'/>
+      </div>
+    </div>
+    <div className="forgot-password">Lost Password? <span>Click Here !</span></div>
+    <div className="submit-container">
+      <div className={action==="Login"?"submit gray":"submit"}>Sign Up</div>
+      <div className={action==="Sign Up"?"submit gray":"submit"}>Login</div>
+    </div>
+  </div>
+  )
+}
+
+export default login
