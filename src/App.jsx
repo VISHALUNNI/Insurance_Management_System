a825833bc37832bdcf9cb3bf0f3fb8ea3b1f8cfb
VISHALUNNI <vishalunni87@gmail.com>
2023-10-04 07:58:35 +0530

    Home added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import Login from './assets/login.jsx'; 
-import './app.css'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
-const App=()=>{
-	return(
-		<div>
-		<Login/>
-		</div>
-	)
+// pages
+import Home from "./pages/home"
+import Login from './pages/login'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <h1>Supa Smoothies</h1>
+        <Link to="/">Home</Link>
+        <Link to="/Login">Login/Signup</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App 
+export default App;
