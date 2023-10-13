ce09fbc4963f51e1755dcd69d3c02a48f1ad69b8
VISHALUNNI <vishalunni87@gmail.com>
2023-10-14 00:10:02 +0530

    Home page edited

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 // pages
 import Home from "./pages/home"
 import Login from './pages/login'
+import './app.css'
 
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <h1>Supa Smoothies</h1>
+      <nav className="nav">
+      <h1>Insurance Management</h1>
+        <div className="nav_container">
         <Link to="/">Home</Link>
         <Link to="/Login">Login/Signup</Link>
+        <Link to="Claim">Claim</Link>
+        </div>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
