60bec304ec6c954369ab28fd5e716f10507e4050
VISHALUNNI <vishalunni87@gmail.com>
2023-10-15 13:43:24 +0530

    PolicyPage added

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,28 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/home"
 import Login from './pages/login'
 import './app.css'
+import PolicyPage from './pages/PolicyPage';
+import ClaimsPage from "./pages/ClaimsPage";
+import HealthInsurancePage from "./pages/HealthInsurancePage";
+import VehicleInsurancePage from "./pages/VehicleInsurancePage";
 
 
 function App() {
   return (
     <BrowserRouter>
-      <nav className="nav">
-      <h1>Insurance Management</h1>
-        <div className="nav_container">
-        <Link to="/">Home</Link>
-        <Link to="/Login">Login/Signup</Link>
-        <Link to="Claim">Claim</Link>
-        </div>
+      <nav className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/claims">Manage Claims</Link>
+      <Link to="/policies">Manage Policies</Link>
+      <Link to="/login">Login</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/claims" element={<ClaimsPage/>}/>
+        <Route path="/policies" element={<PolicyPage/>} />
+        <Route path="/health-insurance" element={<HealthInsurancePage/>}/>
+        <Route path="/vehicle-insurance" element={<VehicleInsurancePage/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/HealthInsurancePage.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+const HealthInsurancePage = () => {
+  return (
+    <div>
+      <h1>Health Insurance</h1>
+      <p>
+        Health insurance is a vital form of coverage that helps protect your
+        financial well-being and health by providing assistance with medical
+        expenses, doctor visits, hospital stays, and prescription medications.
+      </p>
+      <p>
+        Health insurance is an essential safety net that ensures you can
+        receive proper medical care when needed without facing substantial
+        out-of-pocket costs.
+      </p>
+      <h3>Key Features of Health Insurance:</h3>
+      <ul>
+        <li>
+          <strong>Medical Consultations and Treatments:</strong> Health
+          insurance covers costs related to doctor consultations, diagnostic
+          tests, and medical treatments.
+        </li>
+        <li>
+          <strong>Hospitalization and Surgery Coverage:</strong> It provides
+          financial support for inpatient hospital care and surgical procedures.
+        </li>
+        <li>
+          <strong>Prescription Drug Coverage:</strong> Health insurance often
+          includes coverage for prescription medications, making them more
+          affordable.
+        </li>
+        <li>
+          <strong>Preventive Care Services:</strong> It encourages preventive
+          healthcare, including vaccinations and regular check-ups.
+        </li>
+        <li>
+          <strong>Mental Health and Maternity Coverage:</strong> Many health
+          insurance plans offer mental health services and maternity coverage.
+        </li>
+      </ul>
+      <h3>Types of Health Insurance Plans:</h3>
+      <p>
+        There are different types of health insurance plans, each with its
+        unique features:
+      </p>
+      <ul>
+        <li>
+          <strong>Health Maintenance Organization (HMO):</strong> HMOs typically
+          require you to select a primary care physician and get referrals to
+          see specialists. They often have lower premiums but require
+          gatekeeping.
+        </li>
+        <li>
+          <strong>Preferred Provider Organization (PPO):</strong> PPOs offer a
+          more extensive network of doctors and specialists, with the option to
+          see out-of-network providers for a higher cost.
+        </li>
+        <li>
+          <strong>Point of Service (POS):</strong> POS plans combine features of
+          HMO and PPO plans. They require you to choose a primary care provider
+          but allow referrals for specialists.
+        </li>
+        <li>
+          <strong>High Deductible Health Plan (HDHP):</strong> HDHPs have higher
+          deductibles but lower premiums. They're often paired with Health
+          Savings Accounts (HSAs) for tax advantages.
+        </li>
+      </ul>
+      <p>
+        When selecting a health insurance plan, it's crucial to evaluate your
+        healthcare needs, budget, and preferences. Consider factors such as
+        premiums, deductibles, copayments, and the network of healthcare
+        providers.
+      </p>
+    </div>
+  );
+};
+
+export default HealthInsurancePage;
