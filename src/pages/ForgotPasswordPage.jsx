444340b0e3094e40c55c027e306ad9de9b81f8c4
Vishal Unni <vishalunni87@gmail.com>
2023-11-17 10:30:44 +0530

    Update ForgotPasswordPage.jsx

--- src/pages/ForgotPasswordPage.jsx
+++ src/pages/ForgotPasswordPage.jsx
@@ -18,7 +18,7 @@ const ForgotPasswordPage = () => {
     try {
     
       const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: 'http://localhost:5173/#/reset-password', 
+        redirectTo: 'https://imsappp.vercel.app/#/reset-password', 
       });
   
       if (error) {
