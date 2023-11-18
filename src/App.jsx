90377beaf37ef708963e96070f01d173577671e1
VISHALUNNI <vishalunni87@gmail.com>
2023-11-18 21:30:51 +0530

    navabar

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,22 @@ function NavbarAuthenticated() {
       <Link to="/health-insurance">Purchase Health Insurance</Link>
       <Link to="/vehicle-insurance">Purchase Vehicle Insurance</Link>
       <Link to="/dashboard">Dashboard</Link>
+<<<<<<< HEAD
+      <div className="navbar-profile">
+      <div className="profile-icon" onClick={toggleDropdown}>
+        {/* You can use an icon here, e.g., user icon */}
+        🧑
+      </div>
+      {showDropdown && (
+        <div className="profile-dropdown">
+          <button onClick={handleUpdateProfile}>Update Profile</button>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      )}
+    </div>
+=======
+      </div>
+>>>>>>> 24cf965a1b18f0ba0bbba7c1d1751c31ac0f5112
     </>
   );
 }
