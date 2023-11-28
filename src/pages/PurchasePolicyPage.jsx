4377f142107a7ea6e7bc5c63fb93b92254e62a98
hacksh4w <mrinalsnair@gmail.com>
2023-11-28 17:57:06 +0530

    style : purchase policy buttons

--- src/pages/PurchasePolicyPage.jsx
+++ src/pages/PurchasePolicyPage.jsx
@@ -20,7 +20,7 @@ const PurchasePolicyPage = () => {
       <div className='kk'>
         <h2>Purchase Policy</h2>
         <p>Select the type of policy you want to purchase:</p>
-        <div>
+        <div className='policy-buttons'>
           {/* Link to a new page for handling policy selection */}
           <Link to="/purchase-details?policyType=Health">
             <button>Health Insurance</button>
