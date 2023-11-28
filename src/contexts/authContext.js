99bf048ae9e422b7b9119b14f00f8f2b9292884b
hacksh4w <mrinalsnair@gmail.com>
2023-11-28 20:50:19 +0530

    refactor

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from 'react';
-import supabase from './config/SupabaseClient';
+import supabase from '../config/SupabaseClient';
 
 const AuthContext = createContext();
 
